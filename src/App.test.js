760359881bcdd6d9383ba94103f29eceacdb939f
Devagiri Rohan <61906434+Devagiri-Rohan@users.noreply.github.com>
2024-03-20 13:03:26 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+          const linkElement = screen.getByText(/AZURE CXP RESEARCH COPILOT/i);
   expect(linkElement).toBeInTheDocument();
 });
